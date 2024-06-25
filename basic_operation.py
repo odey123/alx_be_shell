a51681b3644fbec69427b90620f50a7d425dc765
Jude Odey <ohotujude@gmail.com>
2024-06-25 10:58:07 +0100

    pls work

---
+++ basic_operation.py
@@ -0,0 +1,10 @@
+number1 = 12
+number2 = 8
+
+result1 = (number1 + number2)
+result2 = (number1 - number2)
+result3 = (number1 * number2)
+
+print(f"Addition of {number1} and {number2} is {result1} ")
+print(f"Subtraction of {number1} and {number2} is {result2} ")
+print(f"Multiplication of {number1} and {number2} is {result1} ")
